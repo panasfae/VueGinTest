37add1da8f4a6f011c4a81469e814d9fde5069b8
Richard Leung <liang_yuxuan@163.com>
2019-06-22 18:16:29 +0800

    初始提交

---
+++ golang/main.go
@@ -0,0 +1,48 @@
+package main
+
+import(
+	_"fmt"
+	"github.com/gin-gonic/gin"
+	"math/rand"
+	"net/http"
+)
+
+func HelloPage(c *gin.Context){
+	c.JSON(http.StatusOK, gin.H{
+		"message":"Welcome to bgops, please visit https://xxbandy.github.io!",
+	})
+}
+
+func main(){
+	r:=gin.Default()
+	v1:=r.Group("/v1")
+	{
+		v1.GET("/hello", HelloPage)
+		v1.GET("/hllo/:name", func(c *gin.Context){
+			name:=c.Param("name")
+			c.String(http.StatusOK, "Hello %s", name)
+		})
+		
+		v1.GET("/line", func(c *gin.Context){
+			// 注意:在前后端分离过程中，需要注意跨域问题，因此需要设置请求头
+			c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+			legendData:=[]string{"周一", "周二", "周三", "周四", "周五", "周六", "周日"}
+			xAxisData:=[]int{120, 240, rand.Intn(500), rand.Intn(500), 150, 230, 180}
+			c.JSON(200, gin.H{
+				"legend_data":legendData,
+				"xAxisData":xAxisData,
+			})
+		})
+
+		//定义默认路由
+		r.NoRoute(func(c *gin.Context){
+			c.JSON(http.StatusNotFound, gin.H{
+				"status":404,
+				"error": "404, page not exits!",
+			})
+		})
+		r.Run(":8080")
+
+
+	}
+}
